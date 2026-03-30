c4f2e37b387a82bc9e909d120d939e493e8f5a53
Nathalie M. Castro Mojica <nathalie.castro@upr.edu>
2026-03-30 01:13:05 -0400

    implemented desktop garden apps

---
+++ ultimate-game-jam-/scripts/desktop_garden_2.gd
@@ -0,0 +1,32 @@
+extends Control 
+
+@onready var progressBar = $window/TextureProgressBar
+@onready var waterButton = $window/water
+@onready var inspopanel = $Inspo
+@onready var window = $window
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_water_pressed():
+	progressBar.value += 10
+	
+	if progressBar.value >= 100:
+		_show_inspo_quote()
+
+func _show_inspo_quote():
+	waterButton.disabled = true
+	inspopanel.visible = true 
+	SoundManager.victory_play()
+
+func _on_exit_pressed():
+	window.visible = false 
+
+func _on_exit_inspo_pressed():
+	inspopanel.visible = false 
--- ultimate-game-jam-/scripts/sound_manager.gd
+++ ultimate-game-jam-/scripts/sound_manager.gd
@@ -4,17 +4,20 @@ var send_player = AudioStreamPlayer.new()
 var buddy_in_player = AudioStreamPlayer.new()
 var buddy_out_player = AudioStreamPlayer.new()
 var rcv_player = AudioStreamPlayer.new()
+var victory_player = AudioStreamPlayer.new()
 
 func _ready():
 	add_child(send_player)
 	add_child(buddy_in_player)
 	add_child(buddy_out_player)
 	add_child(rcv_player)
+	add_child(victory_player)
 	
 	send_player.stream = load("res://assets/sounds/aimsend.wav")
 	buddy_in_player.stream = load("res://assets/sounds/buddyin.wav")
 	buddy_out_player.stream = load("res://assets/sounds/buddyout (1).wav")
 	rcv_player.stream = load("res://assets/sounds/aimrcv.wav")
+	victory_player.stream = load("res://assets/sounds/fairy sound.wav")
 
 func send_play():
 	if send_player.stream:
@@ -31,3 +34,7 @@ func buddyout_play():
 func rcv_play():
 	if rcv_player.stream and !Dialogue.is_day_over:
 		rcv_player.play()
+
+func victory_play():
+	if victory_player.stream:
+		victory_player.play()
